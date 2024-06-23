5f863b90aa4a1bff85edded7389042318c3a1bb9
kepobadai <musabsidqi@gmail.com>
2024-06-23 12:06:32 +0700

    Add files via upload

---
+++ khodam otomatis.py
@@ -0,0 +1,79 @@
+print("")
+print('cek khodam gratis!')
+print("")
+while True:
+	print("")
+	list1=input('masukan nama : ')
+	c=int(input('tanggal lahir : '))
+	a=len(list1)
+	b=a+c-2
+	if b==1:
+		print("khodam kamu adalah babi")
+	elif b==2:
+		print("khodam kamu adalah kucing")
+	elif b==3:
+	   print("khodam kamu adalah paus")
+	elif b==4:
+	   print("khodam kamu adalah lumba-lumba")
+	elif b==5:
+	   print("khodam kamu adalah elang")
+	elif b==6:
+	   print("khodam kamu adalah buaya")
+	elif b==7:
+		print("khodam kamu adalah rusa")
+	elif b==8:
+		print("khodam kamu adalah kuda")
+	elif b==9:
+	   print("khodam kamu adalah gorilla")
+	elif b==10:
+	   print("khodam kamu adalah kodok")
+	elif b==11:
+	   print("khodam kamu adalah kakatua")
+	elif b==12:
+	   print("khodam kamu adalah singa")
+	elif b==22:
+		print("khodam kamu adalah badak")
+	elif b==13:
+	   print("khodam kamu adalah sapi hitam")
+	elif b==14:
+	   print("khodam kamu adalah aligator")
+	elif b==15:
+	   print("khodam kamu adalah harimau")
+	elif b==16:
+	   print("khodam kamu adalah keledai")
+	elif b==17:
+		print("khodam kamu adalah ayam betot")
+	elif b==18:
+		print("khodam kamu adalah celeng")
+	elif b==19:
+	   print("khodam kamu adalah gorilla")
+	elif b==20:
+	   print("khodam kamu adalah kodok")
+	elif b==21:
+	   print("khodam kamu adalah kakatua")
+	elif b==23:
+	   print("khodam kamu adalah bunglon")
+	elif b==32:
+		print("khodam kamu adalah kucing")
+	elif b==33:
+	   print("khodam kamu adalah paus")
+	elif b==24:
+	   print("khodam kamu adalah lumba-lumba")
+	elif b==25:
+	   print("khodam kamu adalah elang")
+	elif b==26:
+	   print("khodam kamu adalah buaya")
+	elif b==27:
+		print("khodam kamu adalah rusa")
+	elif b==28:
+		print("khodam kamu adalah kuda")
+	elif b==29:
+	   print("khodam kamu adalah gorilla")
+	elif b==30:
+	   print("khodam kamu adalah kodok")
+	elif b==31:
+	   print("khodam kamu adalah kakatua")
+	elif b==34:
+	   print("khodam kamu adalah singa")
+	else:
+		print("khodam kamu adalah gajah hitam")
